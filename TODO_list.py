a6cfdfe264ec688ae92cd592b304c4ebfea177b2
adarshvinod-devs <adarsh2047n@gmail.com>
2026-07-23 21:00:59 +0530

    Created 'view_task' function

--- TODO_list.py
+++ TODO_list.py
@@ -1,18 +1,32 @@
 from datetime import datetime , date
 import json
 
-task_list = {}
+try:
+    with open("TODO.json","r") as file:
+        task_list = json.load(file)
 
-def create_task():
+except FileNotFoundError:
+    task_list = {}
+
+def add_task():
     task_name = input("Enter task name: ")
     task_discription = input("Enter task discription: ")
     date_str = input("Enter due date(DD-MM-YYYY): ")
-    #due_date = datetime.strptime(date_str,"%d-%m-%Y").date()
-    created_date = date.today()
+    created_date = date.today().strftime("%d-%m-%Y")
     task_list[task_name] = {"Discription":task_discription,"Due Date":date_str,"Task added on":str(created_date)}
     with open("TODO.json","w") as file:
         json.dump(task_list,file)
 
+def view_task():
+    sorted_task_list = sort_task()
+    for  key,value in sorted_task_list:
+        print(f"• {key}")
+        print("============================")
+        for nested_key,nested_value in value.items():
+            print(f"{nested_key} : {nested_value}")
 
-create_task()
+def sort_task():
+    sorted_task_list = sorted(task_list.items(), key= lambda item: datetime.strptime(item[1]["Due Date"],"%d-%m-%Y").date())
+    return sorted_task_list
 
+view_task()
