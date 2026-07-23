1b93ee32e88719eb9a0e7e6f310f1eefbd727d1c
adarshvinod-devs <adarsh2047n@gmail.com>
2026-07-23 21:13:15 +0530

    Added 'remove_task' function

--- TODO_list.py
+++ TODO_list.py
@@ -9,7 +9,7 @@ except FileNotFoundError:
     task_list = {}
 
 def add_task():
-    task_name = input("Enter task name: ")
+    task_name = input("Enter task title: ")
     task_discription = input("Enter task discription: ")
     date_str = input("Enter due date(DD-MM-YYYY): ")
     created_date = date.today().strftime("%d-%m-%Y")
@@ -23,10 +23,19 @@ def view_task():
         print(f"• {key}")
         print("============================")
         for nested_key,nested_value in value.items():
-            print(f"{nested_key} : {nested_value}")
+            print(f"{nested_key} : {nested_value}\n")
 
 def sort_task():
     sorted_task_list = sorted(task_list.items(), key= lambda item: datetime.strptime(item[1]["Due Date"],"%d-%m-%Y").date())
     return sorted_task_list
+
+def remove_task():
+    name = input("Enter task title to remove: ")
+    if name in task_list:
+        del task_list[name]
+        with open ("TODO.json","w") as file:
+            json.dump(task_list,file)
+    else:
+        print("Couldn't find the task with that title")
 
 view_task()
